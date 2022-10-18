d93c27db71ef7e5ae0f2232c68e6692731cd6b84
anologicon <pauloraimundi2009@gmail.com>
2022-10-17 22:28:57 -0300

    feat: fetch matchs id's by sommner

--- data-colector/main.py
+++ data-colector/main.py
@@ -1,9 +1,42 @@
+import logging
 from service.league_of_legends_service import LeagueOfLegendsService
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
 def main():
+    print("Collecting challengers BR data - Max 200")
     league_of_legends_service = LeagueOfLegendsService()
-    print(league_of_legends_service.fetch_challengers_summoners())
+    summoners_list = league_of_legends_service.fetch_challengers_summoners()
+    summoner_with_details = []
+    for summoner in summoners_list:
+        print(f"Summoner details from {summoner['summonerName']}")
+        summoner_data = {
+            'summoner_detail': league_of_legends_service.fetch_summoner_details(summoner),
+            'summoner_data': summoner
+        }
+        summoner_with_details.append(summoner_data)
+    summoner_matchs = {}
+    for summoner in summoner_with_details:
+        keep_request = True
+        request_index = 0
+        while(keep_request):
+            match_id_list = league_of_legends_service.fetch_summoners_match_ids(
+                summoner=summoner['summoner_detail'],
+                request_index=request_index
+            )
+            if summoner["summoner_data"]["summonerId"] not in summoner_matchs:
+                summoner_matchs[summoner["summoner_data"]["summonerId"]] = match_id_list
+            else:
+                summoner_matchs[summoner["summoner_data"]["summonerId"]].extend(match_id_list)
+            request_index += 100
+            if len(match_id_list) == 0:
+                keep_request = False
+            
+        
     
+
+
 
 if __name__ == "__main__":
     main()
--- data-colector/service/league_of_legends_service.py
+++ data-colector/service/league_of_legends_service.py
@@ -1,28 +1,58 @@
 from requests import get, Response
+import requests
 from dotenv import load_dotenv
 import os
 from typing import List
-
+from backoff import on_exception, expo
+import ratelimit
+import logging
 load_dotenv()
 
 class LeagueOfLegendsService:
     
+    BASE_BR_URL = "https://br1.api.riotgames.com/lol/"
+    BASE_AMERICA_URL = "https://americas.api.riotgames.com/lol/"
     RANKED_SOLO_5x5_BASE_ENDPOINT = "league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5"
+    SUMMONER_DATA = "summoner/v4/summoners/by-name/{}"
+    MATCHS_IDS = "match/v5/matches/by-puuid/{}/ids?type=ranked&start={}&count={}"
     
     def __init__(self):
         self.default_header = {
             "content-type": "json",
             "X-Riot-Token": os.environ["LOL_API_KEY"],
         }
-        self.base_url = "https://br1.api.riotgames.com/lol/"
 
+    @on_exception(expo, ratelimit.exception.RateLimitException, max_tries=10)
+    @ratelimit.limits(calls=29, period=30)
+    @on_exception(expo, requests.exceptions.HTTPError, max_tries=10)
     def fetch_challengers_summoners(self) -> List:
         return self.__base_request(
-            self.__mount_url(self.RANKED_SOLO_5x5_BASE_ENDPOINT)
+            self.__mount_br_url(self.RANKED_SOLO_5x5_BASE_ENDPOINT)
         )["entries"]
+    
+    @on_exception(expo, ratelimit.exception.RateLimitException, max_tries=10)
+    @ratelimit.limits(calls=1600, period=60)
+    @on_exception(expo, requests.exceptions.HTTPError, max_tries=10)
+    def fetch_summoner_details(self, summoner):
+        return self.__base_request(
+            self.__mount_br_url(self.SUMMONER_DATA.format(
+                summoner["summonerName"]))
+        )
+    
+    @on_exception(expo, ratelimit.exception.RateLimitException, max_tries=10)
+    @ratelimit.limits(calls=250, period=10)
+    @on_exception(expo, requests.exceptions.HTTPError, max_tries=10)
+    def fetch_summoners_match_ids(self, summoner, request_index = 0, max_return = 100) -> List:
+        return self.__base_request(
+            self.__mount_americas_url(self.MATCHS_IDS.format(
+                summoner["puuid"], request_index, max_return))
+        )
 
-    def __mount_url(self, end_point: str) -> str:
-        return self.base_url + end_point
+    def __mount_americas_url(self, end_point: str) -> str:
+        return self.BASE_AMERICA_URL + end_point
+
+    def __mount_br_url(self, end_point: str) -> str:
+        return self.BASE_BR_URL + end_point
 
     def __base_request(self, url: str):
         response = get(url, headers=self.default_header)
