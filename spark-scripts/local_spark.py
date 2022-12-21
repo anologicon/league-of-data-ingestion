01fc84d208b86fa9d26d509db083e99765fef555
anologicon <pauloraimundi2009@gmail.com>
2022-12-21 00:22:12 -0300

    feat: spark docker local cluster

---
+++ spark-scripts/local_spark.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql import functions as F
+from pyspark.sql.functions import regexp_extract
+from functools import partial
+
+spark = SparkSession.builder.appName("ToParquetLeagueOfData").master("spark://localhost:7077") \
+    .config("spark.jars.packages", "com.amazonaws:aws-java-sdk:1.11.563,com.amazonaws:aws-java-sdk-bundle:1.11.874,org.apache.hadoop:hadoop-aws:3.3.2") \
+    .config("spark.hadoop.fs.s3a.multipart.size", "838860800") \
+    .config('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider') \
+    .config(
+        "spark.hadoop.fs.s3a.path.style.access",
+        True
+    ).getOrCreate()
+spark.sparkContext.setLogLevel("ERROR")
+sc = spark
+hadoop_conf = sc._jsc.hadoopConfiguration()
+hadoop_conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+hadoop_conf.set("fs.s3a.endpoint", "http://192.168.0.10:9000")
+hadoop_conf.set("fs.s3a.access.key", "OhLfedtD1ulumaig")
+hadoop_conf.set("fs.s3a.secret.key", "IuWsU1j3YvArUAS26VDOvRPnLYLWUXBo")
+hadoop_conf.set("fs.s3a.path.style.access", "True")
+
+summoners_df = spark.read.json("s3a://league-of-data-raw/")
--- spark-scripts/to_spark.py
+++ spark-scripts/to_spark.py
@@ -5,21 +5,17 @@ from pyspark.sql import functions as F
 from pyspark.sql.functions import regexp_extract
 from functools import partial
 
-spark = SparkSession.builder.appName("ToParquetLeagueOfData").master("spark://localhost:7077") \
-    .config("spark.jars.packages", "com.amazonaws:aws-java-sdk:1.11.563,com.amazonaws:aws-java-sdk-bundle:1.11.874,org.apache.hadoop:hadoop-aws:3.3.2") \
-    .config("spark.hadoop.fs.s3a.multipart.size", "838860800") \
-    .config('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider') \
-    .config(
-        "spark.hadoop.fs.s3a.path.style.access",
-        True
-    ).getOrCreate()
+spark = SparkSession.builder.appName("ToParquetLeagueOfData").master("spark://spark-master:7077").getOrCreate()
+spark.sparkContext.setLogLevel("ERROR")
 sc = spark
 hadoop_conf = sc._jsc.hadoopConfiguration()
 hadoop_conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-hadoop_conf.set("fs.s3a.endpoint", "http://localhost:9000")
-hadoop_conf.set("fs.s3a.access.key", "iiEQfEr51Bxr7SD1")
-hadoop_conf.set("fs.s3a.secret.key", "o3oIo7siSWB1srmQNM7JhYT9fTgttONc")
+hadoop_conf.set("fs.s3a.endpoint", "http://minio:9000")
+hadoop_conf.set("fs.s3a.access.key", "OhLfedtD1ulumaig")
+hadoop_conf.set("fs.s3a.secret.key", "IuWsU1j3YvArUAS26VDOvRPnLYLWUXBo")
 hadoop_conf.set("fs.s3a.path.style.access", "True")
 
-parDF2=spark.read.json("s3a://league-of-data-raw/")
-print(f"CONTAGEM: {parDF2.count()}")
+summoners_df = spark.read.json("s3a://league-of-data-raw/")
+details = summoners_df.select("summoner_detail")
+
+details.write.parquet("s3a://league-of-data-raw/silver/details.parquet")
