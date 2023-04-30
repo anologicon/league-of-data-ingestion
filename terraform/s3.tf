4be43ff857235b1d2f6753cb775a2673a7a6e8f4
anologicon <pauloraimundi2009@gmail.com>
2023-04-30 19:01:33 -0300

    ci: add s3 buckets configs

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    profile = "default"
+    profile = "terraform"
     region = "us-east-1"
 }
 
@@ -13,4 +13,40 @@ resource "aws_s3_bucket" "league-data-silver" {
 
 resource "aws_s3_bucket" "league-data-gold" {
   bucket = "league-data-gold"
+}
+
+resource "aws_s3_bucket_acl" "league-data-bronze-acl" {
+  bucket = aws_s3_bucket.league-data-bronze.id
+  acl  = "private"
+}
+
+resource "aws_s3_bucket_acl" "league-data-silver-acl" {
+  bucket = aws_s3_bucket.league-data-silver.id
+  acl  = "private"
+}
+
+resource "aws_s3_bucket_acl" "league-data-gold-acl" {
+  bucket = aws_s3_bucket.league-data-gold.id
+  acl  = "private"
+}
+
+resource "aws_s3_bucket_versioning" "league-data-bronze-versioning" {
+  bucket = aws_s3_bucket.league-data-bronze.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "league-data-silver-versioning" {
+  bucket = aws_s3_bucket.league-data-silver.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "league-data-gold-versioning" {
+  bucket = aws_s3_bucket.league-data-gold.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
